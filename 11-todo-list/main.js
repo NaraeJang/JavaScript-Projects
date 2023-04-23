e71126ad8252c910a39f4cfb52c286d9dc19d06f
NaraeJang <j.naaaarae@gmail.com>
2023-04-23 12:29:35 -0400

    Update 11-todo-list html and js

--- 11-todo-list/main.js
+++ 11-todo-list/main.js
@@ -1,14 +1,29 @@
-const todoOne = [];
+const todoOne = ["make dinner", "wash dishes"];
 
+renderTodoList();
+
+// Generating the HTML
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoOne.length; i++) {
+    const todoOneFinalValue = todoOne[i];
+    const html = `<p>${todoOneFinalValue}</p>`;
+
+    todoListHTML += html;
+    console.log(todoListHTML);
+    document.querySelector(".js-container").innerHTML = todoListHTML;
+  }
+}
+
+// Update TodoList
 function updateTodoOne() {
   const todoOneValue = document.querySelector(".todoOneInput");
   todoOne.push(todoOneValue.value);
 
   console.log(todoOne);
 
-  for (let i = 0; i < todoOne.length; i++) {
-    const todoOneFinalValue = todoOne[i];
-  }
-
   todoOneValue.value = "";
+
+  renderTodoList();
 }
