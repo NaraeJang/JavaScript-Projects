2e788aa7c8fd33e47b41e4a81c2f23751ba8e2f2
NaraeJang <j.naaaarae@gmail.com>
2023-04-26 20:28:18 -0400

    Update forloop to foreach()

---
+++ 11-todo-list/12-main.js
@@ -0,0 +1,36 @@
+const todoOne = ["make dinner", "wash dishes"];
+
+renderTodoList();
+
+// Generating the HTML
+function renderTodoList() {
+  let todoListHTML = "";
+
+  // for (let i = 0; i < todoOne.length; i++) {
+  //   const todoOneFinalValue = todoOne[i];
+  //   const html = `<p>${todoOneFinalValue}</p>`;
+
+  //   todoListHTML += html;
+  //   console.log(todoListHTML);
+  //   document.querySelector(".js-container").innerHTML = todoListHTML;
+  // }
+
+  todoOne.forEach((value) => {
+    html = `<p>${value}</p>`;
+    todoListHTML += html;
+    console.log(todoListHTML);
+    document.querySelector(".js-container").innerHTML = todoListHTML;
+  });
+}
+
+// Update TodoList
+function updateTodoOne() {
+  const todoOneValue = document.querySelector(".todoOneInput");
+  todoOne.push(todoOneValue.value);
+
+  console.log(todoOne);
+
+  todoOneValue.value = "";
+
+  renderTodoList();
+}
---
+++ 12-TodoList/12-main.js
@@ -0,0 +1,66 @@
+const todoList = JSON.parse(localStorage.getItem("todoList")) || [
+  {
+    name: "Good for you",
+    dueDate: "2022-12-11",
+  },
+];
+
+generateHTML();
+
+function generateHTML() {
+  let inputHTML = "";
+
+  // for (let i = 0; i < todoList.length; i++) {
+  //   const todoItem = todoList[i];
+  //   const { name, dueDate } = todoItem;
+  //   const html = `
+  //   <div>${name}</div>
+  //   <div>${dueDate}</div>
+  //   <button onclick="
+  //       todoList.splice(${i}, 1);
+  //      generateHTML();
+  //       // Whenever we update the todo list, save in localStorage.
+  //       saveToStorage();
+  //     " class="delete-todo-button">Delete</button> `;
+  //   inputHTML += html;
+  // }
+
+  todoList.forEach((value, index) => {
+    console.log(value);
+    const { name, dueDate } = value;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+        todoList.splice(${index}, 1);
+       generateHTML();
+        // Whenever we update the todo list, save in localStorage.
+        saveToStorage();
+      " class="delete-todo-button">Delete</button> `;
+    inputHTML += html;
+  });
+
+  document.querySelector(".js-todo-list").innerHTML = inputHTML;
+}
+
+function addTodo() {
+  let todoName = document.querySelector(".js-name-input");
+  let todoDate = document.querySelector(".js-due-date-input");
+  const name = todoName.value;
+  const dueDate = todoDate.value;
+
+  todoList.push({
+    name,
+    dueDate,
+  });
+
+  console.log(todoList);
+  todoName.value = "";
+
+  generateHTML();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+}
